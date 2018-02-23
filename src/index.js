8dbebb14ea788bbda711727bd21b3289b841f887
Romicusblr <mastroma@mail.ru>
2018-02-24 01:27:34 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const findSimpleDigits = (base) => {
+    let simple = 2; //starting simple digit
+    let result = {};
+    do {
+      if (base % simple === 0) {
+        if (!result[simple]) {
+          result[simple] = 0;
+        }
+        result[simple]++;
+        base /= simple;
+      } else {
+        simple++;
+      }
+    }
+    while (simple <= base);
+    return result;
+  }
+  let simpleObj = findSimpleDigits(base);
+  debugger;
+  let res = 0;
+  for (let key in simpleObj) {
+    let counter = 0;
+    let quantity = Math.floor(Math.log(number) / Math.log(key)); // quantity pow base
+    for (i = 1; i <= quantity; i++) {
+      counter += Math.floor(number / Math.pow(key, i));
+    }
+    counter /= simpleObj[key];
+    if(!res || res>counter){
+      res = counter;
+    }
+
+  }
+  return Math.floor(res);
+
 }
